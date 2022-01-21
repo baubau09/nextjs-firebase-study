c3a2c596956a6d94e4aa39e0a9c4ba901869918f
baubau09 <katietran.182@gmail.com>
2022-01-21 13:57:48 +0700

    login signup page and usercontext

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import {UserContext} from '../lib/context'
+import { Toaster } from 'react-hot-toast'
+import {useEffect, useState} from 'react'
+import {useAuthState} from 'react-firebase-hooks/auth'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <UserContext.Provider value={{user: {}, username: 'jeff123'}}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Toaster/>
+      </UserContext.Provider>
+    </>
+  )
 }
 
 export default MyApp
