05d4d14bc89bc535352e645b8ea41684718c9cf9
baubau09 <katietran.182@gmail.com>
2022-02-04 20:56:25 +0700

    index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,63 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, postToJSON } from '../lib/firebase';
+import { Timestamp, query, where, orderBy, limit, collectionGroup, getDocs, startAfter, getFirestore } from 'firebase/firestore';
+import { useState } from 'react';
 
-export default function Home() {
+
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+
+    const ref = collectionGroup(getFirestore(), 'posts');
+    const postsQuery = query(
+        ref,
+        where('published', '==', true),
+        orderBy('createdAt', 'desc'),
+        limit(LIMIT),
+    )
+
+    const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+    return {
+        props: { posts }, // will be passed to the page component as props
+    };
+}
+
+export default function Home(props) {
+    const [posts, setPosts] = useState(props.posts);
+    const [loading, setLoading] = useState(false);
+    const [postsEnd, setPostsEnd] = useState(false);
+
+    // Get next page in pagination query
+    const getMorePosts = async () => {
+        setLoading(true);
+        const last = posts[posts.length - 1];
+
+        const cursor = typeof last.createdAt === 'number' ? Timestamp.fromMillis(last.createdAt) : last.createdAt;
+
+        const ref = collectionGroup(getFirestore(), 'posts');
+        const postsQuery = query(
+            ref,
+            where('published', '==', true),
+            orderBy('createdAt', 'desc'),
+            startAfter(cursor),
+            limit(LIMIT),
+        )
+
+        const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+
+        setPosts(posts.concat(newPosts));
+        setLoading(false);
+
+        if (newPosts.length < LIMIT) {
+            setPostsEnd(true);
+        }
+    };
+
     return (
         <div className={styles.container}>
             <Head>
@@ -11,7 +66,21 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1>Hello world!</h1>
+            <main>
+                <div className="card card-info">
+                    <h2>💡 Next.js + Firebase - The Full Course</h2>
+                    <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
+                    <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
+                </div>
+
+                <PostFeed posts={posts} />
+
+                {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+                <Loader show={loading} />
+
+                {postsEnd && 'You have reached the end!'}
+            </main>
 
         </div>
     )
